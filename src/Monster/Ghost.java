75f097466c9c5a452dcc7ce07e71ec3ecfcfd0cf
alexhurtak <amh8607@psu.edu>
2026-05-31 21:58:49 -0400

    Second Commit

---
+++ src/Monster/Ghost.java
@@ -0,0 +1,23 @@
+/**
+ * Project: Monster Lab
+ * Purpose Details: Demonstrates abstraction, inheritance, polymorphism, and enums.
+ * Course: IST 242
+ * Author: Alexander Hurtak
+ * Date Developed: 05/31/2026
+ * Last Date Changed: 05/31/2026
+ * Rev: 1
+ */
+
+package Monster;
+
+public final class Ghost extends Monster {
+
+    public Ghost(String name, int health, int level, String color) {
+        super(name, health, level, color);
+    }
+
+    @Override
+    public void specialPowers() {
+        System.out.println(getName() + " haunts enemies using " + getPower() + "!");
+    }
+}
